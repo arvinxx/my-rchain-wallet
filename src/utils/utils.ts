4ca53b5dbbca0512fd16de2050a449e2c74d5733
arvinx <arvinx@foxmail.com>
2019-10-26 02:59:31 +0800

    :bug: Fix data type

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -29,8 +29,9 @@ export const setEncryptedItem = (key: IEncryptedData, value: any) => {
 
 export const getDecryptedItem = (key: IEncryptedData) => {
   const res = localStorage.getItem(key) as string;
-  if (res) {
-    return JSON.parse(decrypt(res));
+  const des = decrypt(res);
+  if (res && des) {
+    return JSON.parse(des);
   }
 };
 export const copyToClipboard = (string: string): boolean => {
